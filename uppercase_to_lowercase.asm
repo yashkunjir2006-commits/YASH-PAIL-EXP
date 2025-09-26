1d37c2d9f432a40a04a3df2485d6d78b9606d6fe
yashkunjir2006-commits <yashkunjir2006@gmail.com>
2025-09-26 09:26:23 +0530

    Create uppercase_to_lowercase.asm

---
+++ uppercase_to_lowercase.asm
@@ -0,0 +1,29 @@
+section .data
+    s1 db "HELLO WORLD",0Ah
+    len equ $-s1
+
+section .bss
+    s2 resb 20
+
+section .text
+    global _start
+
+_start:
+    mov ecx, len
+    mov esi, s1
+    mov edi, s2
+
+loop_here:
+    lodsb
+    or al, 20h
+    stosb
+    loop loop_here
+
+    mov edx, len
+    mov ecx, s2
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+
+    mov eax, 1
+    int 0x80
